6dab23fd4a35ae92659da8c99d92d0915ad3e0b2
Marujinho <aka.contato@gmail.com>
2019-03-26 22:27:53 -0300

    tela home qse pronta

---
+++ assets/js/functions.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    //linkedin
+    $( ".linkedin" ).mouseover(function(){
+        $(this).attr("src", "assets/images/home/linkedin_mo.png");
+    });
+    $( ".linkedin" ).mouseout(function(){
+        $(this).attr("src", "assets/images/home/linkedin.png");
+    });
+
+    //facebook
+    $( ".facebook" ).mouseover(function(){
+        $(this).attr("src", "assets/images/home/fb_mo.png");
+    });
+    $( ".facebook" ).mouseout(function(){
+        $(this).attr("src", "assets/images/home/fb.png");
+    });
+
+    //email
+    $( ".email" ).mouseover(function(){
+        $(this).attr("src", "assets/images/home/email_mo.png");
+    });
+    $( ".email" ).mouseout(function(){
+        $(this).attr("src", "assets/images/home/email.png");
+    });
+
+    //back_to_top
+    $( ".back_to_top" ).mouseover(function(){
+        $(this).attr("src", "assets/images/home/back_to_top_mo.png");
+    });
+    $( ".back_to_top" ).mouseout(function(){
+        $(this).attr("src", "assets/images/home/back_to_top.png");
+    });
+
+
+    //next_project
+    $( ".next_project" ).mouseover(function(){
+        $(this).attr("src", "assets/images/home/next_project_mo.png");
+    });
+    $( ".next_project" ).mouseout(function(){
+        $(this).attr("src", "assets/images/home/next_project.png");
+    });
+
+
+});
