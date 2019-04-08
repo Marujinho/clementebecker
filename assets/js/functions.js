7fd53c24abbb4806df5c4d807acad1c212bcb69c
Marujinho <aka.contato@gmail.com>
2019-04-08 17:55:38 -0300

    algumas arrumacoes finais

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -54,25 +54,88 @@ $(document).ready(function() {
       });
 
     //muda imagem das thumbs qdo passa o mouse
-      $( ".portifolio" ).mouseover(function() {
+      // $( ".portifolio" ).mouseover(function() {
+      //
+      //     let element = $(this).find("img");
+      //     let img = element.first().attr('src');
+      //     var res = img.split(".jpg");
+      //     let newThumb = res[0] + '_mo.jpg' + res[1];
+      //
+      //     element.first().attr('src', newThumb);
+      // });
+      //
+      // $( ".portifolio" ).mouseleave(function() {
+      //
+      //     let element = $(this).find("img");
+      //     let img = element.first().attr('src');
+      //     var res = img.split("_mo");
+      //     let newThumb = res[0] + res[1];
+      //
+      //     element.first().attr('src', newThumb);
+      // });
 
-          let element = $(this).find("img");
-          let img = element.first().attr('src');
-          var res = img.split(".jpg");
-          let newThumb = res[0] + '_mo.jpg' + res[1];
+      $( ".img_1" ).mouseover(function() {
+          $( '.img_1_this' ).attr("src","assets/images/home/TUMB_1_mo.jpg");
+      })
 
-          element.first().attr('src', newThumb);
+      $( ".img_1" ).mouseleave(function() {
+          $( '.img_1_this' ).attr("src","assets/images/home/TUMB_1.jpg");
+      })
 
-      });
+      $( ".img_2" ).mouseover(function() {
+          $( '.img_2_this' ).attr("src","assets/images/home/TUMB_2_mo.jpg");
+      })
 
-      $( ".portifolio" ).mouseleave(function() {
+      $( ".img_2" ).mouseleave(function() {
+          $( '.img_2_this' ).attr("src","assets/images/home/TUMB_2.jpg");
+      })
 
-          let element = $(this).find("img");
-          let img = element.first().attr('src');
-          var res = img.split("_mo");
-          let newThumb = res[0] + res[1];
+      $( ".img_3" ).mouseover(function() {
+          $( '.img_3_this' ).attr("src","assets/images/home/TUMB_3_mo.jpg");
+      })
 
-          element.first().attr('src', newThumb);
-      });
+      $( ".img_3" ).mouseleave(function() {
+          $( '.img_3_this' ).attr("src","assets/images/home/TUMB_3.jpg");
+      })
+
+      $( ".img_4" ).mouseover(function() {
+          $( '.img_4_this' ).attr("src","assets/images/home/TUMB_3_mo.jpg");
+      })
+
+      $( ".img_4" ).mouseleave(function() {
+          $( '.img_4_this' ).attr("src","assets/images/home/TUMB_4.jpg");
+      })
+
+      $( ".img_5" ).mouseover(function() {
+          $( '.img_5_this' ).attr("src","assets/images/home/TUMB_5_mo.jpg");
+      })
+
+      $( ".img_5" ).mouseleave(function() {
+          $( '.img_5_this' ).attr("src","assets/images/home/TUMB_5.jpg");
+      })
+
+      $( ".img_6" ).mouseover(function() {
+          $( '.img_6_this' ).attr("src","assets/images/home/TUMB_6_mo.jpg");
+      })
+
+      $( ".img_6" ).mouseleave(function() {
+          $( '.img_6_this' ).attr("src","assets/images/home/TUMB_6.jpg");
+      })
+
+      $( ".img_7" ).mouseover(function() {
+          $( '.img_7_this' ).attr("src","assets/images/home/TUMB_7_mo.jpg");
+      })
+
+      $( ".img_7" ).mouseleave(function() {
+          $( '.img_7_this' ).attr("src","assets/images/home/TUMB_7.jpg");
+      })
+
+      $( ".img_8" ).mouseover(function() {
+          $( '.img_8_this' ).attr("src","assets/images/home/TUMB_8_mo.jpg");
+      })
+
+      $( ".img_8" ).mouseleave(function() {
+          $( '.img_8_this' ).attr("src","assets/images/home/TUMB_8.jpg");
+      })
 
 });
